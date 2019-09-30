7d45a23b7ba5cce7caf08e7d683140f1ce2d86ea
Park Ju Hyung <qkrwngud825@gmail.com>
2019-09-30 21:52:05 +0900

    lz4: staticify functions
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -322,7 +322,7 @@ int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
  *	or a negative result in case of error
  *
  */
-int LZ4_decompress_safe_partial(const char *source, char *dest,
+static int LZ4_decompress_safe_partial(const char *source, char *dest,
 	int compressedSize, int targetOutputSize, int maxDecompressedSize);
 #endif
 
