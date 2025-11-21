8df9836762837931a2e881ab81378fd108e82664
diudiuyc520 <121587602@qq.com>
2025-11-21 08:55:52 +0800

    Add files via upload

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3936,12 +3936,9 @@ static ssize_t memcg_write_event_control(struct kernfs_open_file *of,
 	buf = endp + 1;
 
 	cfd = simple_strtoul(buf, &endp, 10);
-	if (*endp == '\0')
-		buf = endp;
-	else if (*endp == ' ')
-		buf = endp + 1;
-	else
+	if ((*endp != ' ') && (*endp != '\0'))
 		return -EINVAL;
+	buf = endp + 1;
 
 	event = kzalloc(sizeof(*event), GFP_KERNEL);
 	if (!event)
